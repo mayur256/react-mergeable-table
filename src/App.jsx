9859836f66cc688bf6c5de593e29efaafd4e4e5e
Mayur Upadhayay <mayur@localhost.localdomain>
2024-07-29 00:20:28 +0530

    chore: Bootstrapped react project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>Test app</div>
+  )
+}
+
+export default App
